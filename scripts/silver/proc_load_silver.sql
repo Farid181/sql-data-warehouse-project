9d8c7d10f344f6664a8caba1120d6a41ad543bfb
Farid Aslanli <119242478+Farid181@users.noreply.github.com>
2026-08-01 22:53:02 +0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -211,7 +211,7 @@ BEGIN
 		-- Loading silver.erp_px_cat_g1v2
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
-		TRUNCATE TABLE silver.erp_loc_a101 
+		TRUNCATE TABLE silver.erp_px_cat_g1v2
 		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
 
 		INSERT INTO silver.erp_px_cat_g1v2 
